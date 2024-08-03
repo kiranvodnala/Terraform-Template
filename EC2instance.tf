93b88c0185f34e0cbfb657d0be020e256099b52b
kiran vodnala <144259670+kiranvodnala@users.noreply.github.com>
2024-08-03 09:43:13 +0530

    Create EC2instance.tf

---
+++ EC2instance.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "access key"
+  secret_key = "secret key"
+}
+resource "aws_instance" "k8slave1" {
+  ami           ="ami-003932de22c285676"
+  instance_type = "t2.medium"
+  key_name = "newkey"
+  tags = {
+    Name = "k8master"
+  }
+}
+
+resource "aws_instance" "k8slave2" {
+  ami           ="ami-003932de22c285676"
+  instance_type = "t2.medium"
+  key_name = "newkey"
+  tags = {
+    Name = "k8slave2"
+  }
+}
+resource "aws_instance" "k8slave3" {
+  ami           ="ami-003932de22c285676"
+  instance_type = "t2.medium"
+  key_name = "newkey"
+  tags = {
+    Name = "k8slave3"
+  }
+}  
+  
